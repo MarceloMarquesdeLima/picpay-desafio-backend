57af0528fa2e1f156ca371c267e3081dfd2ce053
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2024-10-04 06:39:40 -0300

    Transsaction

--- src/main/java/com/marcelomarques/picpay_desafio_backend/transaction/TransactionService.java
+++ src/main/java/com/marcelomarques/picpay_desafio_backend/transaction/TransactionService.java
@@ -2,8 +2,10 @@ package com.marcelomarques.picpay_desafio_backend.transaction;
 
 import org.springframework.stereotype.Service;
 
+import com.marcelomarques.picpay_desafio_backend.transaction.Transaction;
 import com.marcelomarques.picpay_desafio_backend.wallet.Wallet;
 import com.marcelomarques.picpay_desafio_backend.wallet.WalletRepository;
+import com.marcelomarques.picpay_desafio_backend.wallet.WalletType;
 
 @Service
 public class TransactionService {
@@ -17,7 +19,9 @@ public class TransactionService {
         this.walletRepository = walletRepository;
     }
 
-    public Transaction creTransaction(Transaction transaction) {
+    public Transaction create(Transaction transaction) {
+
+        validate(transaction);
 
         var newTransaction = transactionRepository.save(transaction);
 
@@ -27,4 +31,15 @@ public class TransactionService {
 
         return  newTransaction;
     }
-    
+
+    /**
+     * @param transaction
+     */
+    public void validate(Transaction transaction){
+        walletRepository.findById(transaction.payee())
+            .map(payee -> walletRepository.findById(transaction.payer()))
+            .map(payer -> payer.type() == WalletType.COMUM.getValue() &&
+                payer.balance().compareTo(transaction.value()) >= 0 &&
+                !payer.id().equals(transaction.payee()) ? transaction : null);
+    }
+}
